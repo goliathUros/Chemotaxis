4179e57467a53c7cbd6fdb5409949a75688b5626
unknown <dayi@697-334-Shelob.sf.k12.internal>
2014-10-02 10:19:13 -0700

    changed the ellipses to little characters

--- Chemotaxis.pde
+++ Chemotaxis.pde
@@ -1,4 +1,66 @@
-Bacteria [] what;
+mrSaturn[] dakota = new mrSaturn[100];
+
+void setup() {
+  smooth();
+  size(500,500);
+  for (int i = 0; i <dakota.length; i++) {
+    dakota[i] = new mrSaturn();
+  }
+}
+
+void draw() {
+  background(178,232,86);
+  for (int i = 0; i <dakota.length; i++) {
+    dakota[i].display();
+    dakota[i].boing();
+  }
+}
+
+class mrSaturn {
+  float noseX, noseY;
+  mrSaturn() {
+    noseX = ((int)(Math.random()*500));
+    noseY = ((int)(Math.random()*500));
+  }
+  void display() {
+    fill(255, 232, 167);
+    ellipse(noseX-8, noseY+2.5, 13, 10); //feet
+    ellipse(noseX+8, noseY+2.5, 13, 10);
+    ellipse(noseX, noseY-7.5, 27, 27); //body
+  
+    fill(0);
+    rectMode(CENTER);
+    rect(noseX-7, noseY-14.5, 8, 2, 3);//left eye
+    float lPupilX = map(mouseX, 0, width, noseX-7, noseX-3);
+    float lPupilY = map(mouseY, 0, height, noseY-11.5, noseY-7.5);
+    ellipse(lPupilX, lPupilY, 5, 5);
+    //ellipse(20, 23, 5, 5);
+    rect(noseX+7, noseY-14.5, 8, 2, 3);//right eye
+    float rPupilX = map(mouseX, 0, width, noseX+3, noseX+7);
+    float rPupilY = map(mouseY, 0, height, noseY-11.5, noseY-7.5);
+    ellipse(rPupilX, rPupilY, 5, 5);
+    //ellipse(30, 23, 5, 5);
+    rect(noseX, noseY-24.5, 2, 9);//hairs
+    line(noseX-17, noseY-7.5, noseX-11, noseY-8.5);
+    line(noseX-17, noseY-4.5, noseX-11, noseY-3.5);
+    line(noseX+11, noseY-6.5, noseX+17, noseY-7.5);
+    line(noseX+11, noseY-3.5, noseX+17, noseY-4.5);
+  
+    fill(255, 232, 167);
+    ellipse(noseX, noseY, 18, 16);//nose
+  
+    fill(255, 0, 0);
+    quad(noseX-5, noseY-27.5, noseX+5, noseY-23.5, noseX+5, noseY-27.5, noseX-5, noseY-23.5);//ribbon
+  }
+  
+  void boing() {
+    noseX = noseX + ((int)(Math.random()*7)-3);
+    noseY = noseY + ((int)(Math.random()*7)-3);
+  }
+}
+
+//Old Work
+/*Bacteria [] what;
 
 void setup()   
 {     
@@ -36,4 +98,4 @@ class Bacteria
 		fill(bacFill);
 		ellipse(posX, posY, 10,10);
 	}
-}
+}*/
--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -14,9 +14,71 @@ import java.io.IOException;
 
 public class Chemotaxis extends PApplet {
 
-Bacteria [] what;
+mrSaturn[] dakota = new mrSaturn[100];
 
-public void setup()   
+public void setup() {
+  smooth();
+  size(500,500);
+  for (int i = 0; i <dakota.length; i++) {
+    dakota[i] = new mrSaturn();
+  }
+}
+
+public void draw() {
+  background(178,232,86);
+  for (int i = 0; i <dakota.length; i++) {
+    dakota[i].display();
+    dakota[i].boing();
+  }
+}
+
+class mrSaturn {
+  float noseX, noseY;
+  mrSaturn() {
+    noseX = ((int)(Math.random()*500));
+    noseY = ((int)(Math.random()*500));
+  }
+  public void display() {
+    fill(255, 232, 167);
+    ellipse(noseX-8, noseY+2.5f, 13, 10); //feet
+    ellipse(noseX+8, noseY+2.5f, 13, 10);
+    ellipse(noseX, noseY-7.5f, 27, 27); //body
+  
+    fill(0);
+    rectMode(CENTER);
+    rect(noseX-7, noseY-14.5f, 8, 2, 3);//left eye
+    float lPupilX = map(mouseX, 0, width, noseX-7, noseX-3);
+    float lPupilY = map(mouseY, 0, height, noseY-11.5f, noseY-7.5f);
+    ellipse(lPupilX, lPupilY, 5, 5);
+    //ellipse(20, 23, 5, 5);
+    rect(noseX+7, noseY-14.5f, 8, 2, 3);//right eye
+    float rPupilX = map(mouseX, 0, width, noseX+3, noseX+7);
+    float rPupilY = map(mouseY, 0, height, noseY-11.5f, noseY-7.5f);
+    ellipse(rPupilX, rPupilY, 5, 5);
+    //ellipse(30, 23, 5, 5);
+    rect(noseX, noseY-24.5f, 2, 9);//hairs
+    line(noseX-17, noseY-7.5f, noseX-11, noseY-8.5f);
+    line(noseX-17, noseY-4.5f, noseX-11, noseY-3.5f);
+    line(noseX+11, noseY-6.5f, noseX+17, noseY-7.5f);
+    line(noseX+11, noseY-3.5f, noseX+17, noseY-4.5f);
+  
+    fill(255, 232, 167);
+    ellipse(noseX, noseY, 18, 16);//nose
+  
+    fill(255, 0, 0);
+    quad(noseX-5, noseY-27.5f, noseX+5, noseY-23.5f, noseX+5, noseY-27.5f, noseX-5, noseY-23.5f);//ribbon
+  }
+  
+  public void boing() {
+    noseX = noseX + ((int)(Math.random()*7)-3);
+    noseY = noseY + ((int)(Math.random()*7)-3);
+  }
+}
+
+//Old Work
+/*Bacteria [] what;
+
+void setup()   
 {     
 	size(500,500);
 	what = new Bacteria[100];
@@ -25,7 +87,7 @@ public void setup()
 		i = i+1;
 	}
 }  
-public void draw()   
+void draw()   
 {
 	background(252, 224, 166);
 	for (int i = 1; i <= what.length; i++) {
@@ -43,16 +105,16 @@ class Bacteria
 		bacFill = color(((int)(Math.random()*256)), ((int)(Math.random()*256)), ((int)(Math.random()*256)));
 	}
 
-	public void move() {
+	void move() {
 		posX = posX + ((int) ((Math.random()*8)-4));
 		posY = posY + ((int) ((Math.random()*8)-4));
 	}
 
-	public void show() {
+	void show() {
 		fill(bacFill);
 		ellipse(posX, posY, 10,10);
 	}
-}
+}*/
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Chemotaxis" };
     if (passedArgs != null) {
